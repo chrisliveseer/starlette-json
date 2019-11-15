0aab0ceb93e42099a79f6187a94174889e0f10e1
Christopher Nguyen <chris@liveseer.com>
2019-11-15 15:07:05 -0500

    ver bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 NAME = 'starlette_json'
-VERSION = '19.11.12'
+VERSION = '19.11.15'
 DESCRIPTION = 'Custom json serializers for the Starlette web framework.'
 URL = 'https://github.com/chrisliveseer/starlette-json'
 EMAIL = 'chris@liveseer.com'
